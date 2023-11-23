8d2f35c580e63574cf55326f12c6ecab3bce0399
Bogdan Torhonskyi <bohdan.torhonskyi@ideus.biz>
2023-11-23 12:36:30 +0200

    settings layouts

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,10 +4,7 @@ import React from "react";
 function Home() {
   return (
     <div>
-      Home System
-      <div>
-        <UserButton afterSignOutUrl="/" />
-      </div>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,26 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
 
-export const metadata = {
-  title: "Next.js 13 tutorial with Clerk",
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description: "A community driven platform for aksing questions",
+  icons: {
+    icon: "/assets/logo/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -11,8 +29,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}>
+      <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable}`}>
         <body>{children}</body>
       </html>
     </ClerkProvider>
